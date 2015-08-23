4d92fb5cc7b82ecc02b1445aa2cb8b6559e591dc
Aaron Cohn <aaron@chatupward.com>
2015-08-23 13:58:23 -0700

    Basic user interface

--- src/aacohn84/elevatorsimulator/Main.java
+++ src/aacohn84/elevatorsimulator/Main.java
@@ -1,8 +1,72 @@
 package aacohn84.elevatorsimulator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        print("Welcome to Elevator Simulator. Please choose a simulation mode:\n" +
+              "\n" +
+              "R - Real-time - Make requests and see how the elevator system reacts in real time.\n" +
+              "\n" +
+              "C - Compressed-time - Specify a sequence of requests for the elevator system to process\n" +
+              "                      and get the results immediately.\n" +
+              "\n" +
+              "> ");
+
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        SimMode simMode = null;
+        String input = stdin.readLine();
+        while (!input.equalsIgnoreCase("C") && !input.equalsIgnoreCase("R")) {
+            print("Please enter 'R' for real-time mode, or 'C' for compressed-time mode.\n\n" +
+                  "> ");
+            input = stdin.readLine();
+        }
+        switch (input.toLowerCase()) {
+        case "c":
+            simMode = SimMode.COMPRESSED_TIME;
+            break;
+        case "r":
+            simMode = SimMode.REAL_TIME;
+            break;
+        default:
+            System.err.println("Invalid simulation mode.");
+            System.exit(1);
+        }
+
+        int numFloors = getIntInRange(stdin, "How many floors in this building?", 2, 16);
+        int travelTime = getIntInRange(stdin, "How much time traveling between floors (in seconds)?", 5, 10);
+        int doorTime = getIntInRange(stdin, "How long do the doors remain open (in seconds)?", 5, 10);
+
+        print("Simulation mode: " + simMode.toString() + "\n");
+        print("Number of floors: " + numFloors + "\n");
+        print("Time between floors: " + travelTime + "s\n");
+        print("Time doors stay open: " + doorTime + "s\n");
+    }
+
+    private static int getIntInRange(BufferedReader stdin, String prompt, int min, int max) throws IOException {
+        print(prompt + " (" + min + '-' + max + ")\n\n" +
+              "> ");
+        Integer num = getInteger(stdin);
+        while (num == null || num < min || num > max) {
+            print("Please choose a number between " + min + " and " + max + ": ");
+            num = getInteger(stdin);
+        }
+        return num;
+    }
+
+    private static Integer getInteger(BufferedReader stdin) throws IOException {
+        String input = stdin.readLine();
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException ignored) {
+            return null;
+        }
+    }
+
+    public static void print(String msg) {
+        System.out.print(msg);
     }
 }
---
+++ src/aacohn84/elevatorsimulator/SimMode.java
@@ -0,0 +1,5 @@
+package aacohn84.elevatorsimulator;
+
+public enum SimMode {
+    REAL_TIME, COMPRESSED_TIME;
+}
